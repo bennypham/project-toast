16eefa3e3ea1ac4a858b12b3eefe29db028b8dc9
bennypham <benny.npham@gmail.com>
2025-10-29 18:15:26 -0700

    added live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast/Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,10 +10,15 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
 	const [message, setMessage] = React.useState('');
 	const [variant, setVariant] = React.useState('notice');
+	const [isToastToggled, setIsToastToggled] = React.useState(false);
 
 	function handleSubmit(event) {
 		event.preventDefault();
-		console.log(message, variant);
+		setIsToastToggled(true);
+	}
+
+	function handleToastDismiss(event) {
+		setIsToastToggled(false);
 	}
 
 	return (
@@ -22,7 +28,14 @@ function ToastPlayground() {
 					<img alt="Cute toast mascot" src="/toast.png" />
 					<h1>Toast Playground</h1>
 				</header>
-
+				{isToastToggled && (
+					<Toast
+						handleToastDismiss={handleToastDismiss}
+						variant={variant}
+					>
+						{message}
+					</Toast>
+				)}
 				<div className={styles.controlsWrapper}>
 					<div className={styles.row}>
 						<label
@@ -39,14 +52,12 @@ function ToastPlayground() {
 								className={styles.messageInput}
 								onChange={(event) => setMessage(event.target.value)}
 							/>
-
 							{/* <p>
 								<strong>Message:</strong>
 								{message || '(empty)'}
 							</p> */}
 						</div>
 					</div>
-
 					<div className={styles.row}>
 						<div className={styles.label}>Variant</div>
 						<div
@@ -71,7 +82,6 @@ function ToastPlayground() {
 									</label>
 								);
 							})}
-
 							{/* <p>
 								<strong>Variant:</strong>
 								{variant || 'undefined'}
